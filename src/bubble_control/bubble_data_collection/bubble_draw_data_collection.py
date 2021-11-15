fixing some typos on drawing_dataset, adding proper delta quaternion, transforming quaternions into axis angle representation

src/bubble_control/bubble_data_collection/bubble_draw_data_collection.py
@@ -8,6 +8,7 @@ import copy
 
 from bubble_utils.bubble_data_collection.bubble_data_collection_base import BubbleDataCollectionBase
 from bubble_control.bubble_drawer.bubble_drawer import BubbleDrawer
+from bubble_control.aux.action_sapces import ConstantSpace, AxisBiasedDirectionSpace
 
 
 class BubbleDrawingDataCollectionBase(BubbleDataCollectionBase):
@@ -129,48 +130,6 @@ class BubbleDrawingDataCollectionBase(BubbleDataCollectionBase):
         data_params['action'] = action_i
 
         return data_params
-
-
-class AxisBiasedDirectionSpace(gym.spaces.Space):
-    """
-    Saple space between [0,2pi) with bias towards the axis directions.
-    On prob_axis, the sample will be along one of the cartesian axis directions, i.e. [0, pi/2, pi, 3pi/2]
-    """
-    def __init__(self, prob_axis, seed=None):
-        self.prob_axis = prob_axis
-        super().__init__((), np.float32, seed)
-
-    def sample(self):
-        p_axis_direction = self.np_random.random() # probability of getting an axis motion
-        if p_axis_direction < self.prob_axis:
-            direction_i = 0.5 * np.pi * np.random.randint(0, 4) # axis direction (0, pi/2, pi/ 3pi/2)
-        else:
-            direction_i = np.random.uniform(0, 2 * np.pi)  # direction as [0, 2pi)
-        return direction_i
-
-    def contains(self, x):
-        return 0 <= x <= 2*np.pi
-
-
-class ConstantSpace(gym.spaces.Space):
-    """
-    Constant space. Only has one possible value. For convenience.
-    """
-    def __init__(self, value, seed=None):
-        self.value = value
-        super().__init__((), np.float32, seed)
-
-    def sample(self):
-        return self.value
-
-    def contains(self, x):
-        return x == self.value
-
-    def __eq__(self, other):
-        return (
-                isinstance(other, ConstantSpace)
-                and self.value == other.value
-        )
 
 
 class BubbleDrawingDataCollection(BubbleDrawingDataCollectionBase):

src/bubble_control/bubble_learning/datasets/bubble_drawing_dataset.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tf.transformations as tr
 
 from bubble_utils.bubble_datasets.bubble_dataset_base import BubbleDatasetBase
 
@@ -30,9 +31,8 @@ class BubbleDrawingDataset(BubbleDatasetBase):
         final_imprint_r = self._get_depth_imprint(undef_fc=undef_fc, def_fc=final_fc, scene_name=scene_name, camera_name='right')
         final_imprint_l = self._get_depth_imprint(undef_fc=undef_fc, def_fc=final_fc, scene_name=scene_name, camera_name='left')
         final_imprint = np.stack([final_imprint_r, final_imprint_l], axis=0)
-        final_wrench = self._get_wrench(fc=init_fc, scene_name=scene_name, frame_id=self.wrench_frame)
+        final_wrench = self._get_wrench(fc=final_fc, scene_name=scene_name, frame_id=self.wrench_frame)
 
-        # TODO: Add frames on sample
         init_tf = self._get_tfs(init_fc, scene_name=scene_name, frame_id=self.tf_frame)
         final_tf = self._get_tfs(final_fc, scene_name=scene_name, frame_id=self.tf_frame)
         init_pos = init_tf[..., :3]
@@ -69,10 +69,12 @@ class BubbleDrawingDataset(BubbleDatasetBase):
 
     def _compute_delta_sample(self, sample):
         # TODO: improve this computation
-        input_keys = ['imprint', 'wrench', 'pos', 'quat']
+        input_keys = ['imprint', 'wrench', 'pos']
         time_keys = ['init', 'final']
         for in_key in input_keys:
             sample['delta_{}'.format(in_key)] = sample['final_{}'.format(in_key)] - sample['init_{}'.format(in_key)]
+        # for quaternion, compute the delta quaternion q_delta @ q_init = q_final <=> q_delta
+        sample['delta_quat'] = tr.quaternion_multiply(sample['final_quat'], tr.quaternion_inverse(sample['init_quat']))
         return sample
 
     def _reshape_sample(self, sample):
@@ -90,8 +92,8 @@ class BubbleDrawingDataset(BubbleDatasetBase):
 # DEBUG:
 
 if __name__ == '__main__':
-    data_name = '/home/mmint/Desktop/drawing_data'
-    dataset = BubbleDrawingDataset(data_name=data_name)
+    data_name = '/home/mmint/Desktop/drawing_data_cartesian'
+    dataset = BubbleDrawingDataset(data_name=data_name, wrench_frame='med_base', tf_frame='grasp_frame')
     print('Dataset Name: ', dataset.name)
     print('Dataset Length:', len(dataset))
     sample_0 = dataset[0]

src/bubble_control/bubble_learning/train_files/bubble_dynamics_residual_trainer.py
@@ -7,13 +7,14 @@ from torch.utils.data import random_split
 
 from bubble_control.bubble_learning.models.bubble_dynamics_residual_model import BubbleDynamicsResidualModel
 from bubble_control.bubble_learning.datasets.bubble_drawing_dataset import BubbleDrawingDataset
+from bubble_control.bubble_learning.aux.orientation_trs import QuaternionToAxis
 
 
 
 if __name__ == '__main__':
 
     # params:
-    data_name = '/home/mmint/Desktop/drawing_data'
+    data_name = '/home/mmint/Desktop/drawing_data_cartesian'
 
     batch_size = 5
     max_epochs = 500
@@ -36,7 +37,8 @@ if __name__ == '__main__':
 
     # Load dataset
     num_workers = 8
-    dataset = BubbleDrawingDataset(data_name=data_name)
+    trs = [QuaternionToAxis()]
+    dataset = BubbleDrawingDataset(data_name=data_name, wrench_frame='med_base', tf_frame='grasp_frame', dtype=torch.float32, transformation=trs)
     train_size = int(len(dataset) * train_fraction)
     val_size = len(dataset) - train_size
     train_data, val_data = random_split(dataset, [train_size, val_size],  generator=torch.Generator().manual_seed(seed))
@@ -50,6 +52,8 @@ if __name__ == '__main__':
         'data_name': data_name,
         'num_train_samples': len(train_data),
         'num_val_samples': len(val_data),
+        'wrench_frame': dataset.wrench_frame,
+        'tf_frame': dataset.tf_frame,
     }
 
     model = BubbleDynamicsResidualModel(input_sizes=sizes,