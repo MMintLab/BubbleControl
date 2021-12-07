54ce23930a1d4e7e7cc4faa124578955189b1695
mr-mikmik <aglaner@hotmail.com>
2021-12-06 19:45:30 -0500

    adding reconstruction to be tested

--- src/bubble_control/bubble_model_control/test_estimating_object_pose_from_model_output.py
+++ src/bubble_control/bubble_model_control/test_estimating_object_pose_from_model_output.py
@@ -2,27 +2,106 @@ import numpy as np
 import os
 import sys
 import torch
+import rospy
+import tf2_ros as tf
+import tf.transformations as tr
+from geometry_msgs.msg import TransformStamped
 
 from bubble_control.bubble_learning.datasets.bubble_drawing_dataset import BubbleDrawingDataset
 from bubble_control.bubble_learning.aux.img_trs.block_downsampling_tr import BlockDownSamplingTr
 from bubble_control.bubble_learning.aux.img_trs.block_upsampling_tr import BlockUpSamplingTr
 from bubble_control.bubble_learning.models.bubble_dynamics_pretrained_ae_model import BubbleDynamicsPretrainedAEModel
 from bubble_control.aux.load_confs import load_bubble_reconstruction_params
-from bubble_control.bubble_pose_estimation.bubble_pc_reconstruction import BubblePCReconsturctorDepth
+from bubble_control.bubble_pose_estimation.bubble_pc_reconstruction import BubblePCReconstructorBase
 from bubble_utils.bubble_tools.bubble_img_tools import unprocess_bubble_img
+from bubble_utils.bubble_tools.bubble_pc_tools import get_imprint_pc
+from mmint_camera_utils.point_cloud_utils import tr_pointcloud
 
 
-
-class BubblePCReconsturctorOfflineDepth(BubblePCReconsturctorDepth):
+class BubblePCReconstructorOfflineDepth(BubblePCReconstructorBase):
     def __init__(self, *args, **kwargs):
-        self.depth_r = None
-        self.depth_l = None
+        self.depth_r = {
+            'img': None,
+            'frame': None,
+        }
+        self.depth_l = {
+            'img': None,
+            'frame': None,
+        }
+        self.camera_info = {
+            'left': None,
+            'right': None,
+        }
+        self.buffer = tf.BufferCore()
+
         super().__init__(*args, **kwargs)
 
-    def _get_depth_imgs(self):
-        depth_r = self.depth_r
-        depth_l = self.depth_l
-        return depth_r, depth_l
+    def reference(self):
+        pass
+
+    def add_tfs(self, tfs_df):
+        for indx, row in tfs_df.iterrows():
+            # pack the tf into a TrasformStamped message
+            q_i = [row['qx'], row['qy'], row['qz'], row['qw']]
+            t_i = [row['x'], row['y'], row['z']]
+            parent_frame_id = row['parent_frame']
+            child_frame_id = row['child_frame']
+            ts_msg_i = self._pack_transform_stamped_msg(q_i, t_i, parent_frame_id=parent_frame_id, child_frame_id=child_frame_id)
+            self.buffer.set_transform(ts_msg_i, 'default_authority')
+
+    def _tr_pc(self, pc, origin_frame, target_frame):
+        ts_msg = self.buffer.lookup_transform_core(origin_frame, target_frame, rospy.Time(0))
+        t, R = self._unpack_transform_stamped_msg(ts_msg)
+        pc_tr = tr_pointcloud(pc, R, t)
+        return pc_tr
+
+    def _unpack_transform_stamped_msg(self, ts_msg):
+        x = ts_msg.transform.translation.x
+        y = ts_msg.transform.translation.y
+        z = ts_msg.transform.translation.z
+        qx = ts_msg.transform.rotation.x
+        qy = ts_msg.transform.rotation.y
+        qz = ts_msg.transform.rotation.z
+        qw = ts_msg.transform.rotation.w
+        q = np.array([qx, qy, qz, qw])
+        t = np.array([x, y, z])
+        R = tr.quaternion_matrix(q)
+        return t, R
+
+    def _pack_transform_stamped_msg(self, q, t, parent_frame_id, child_frame_id):
+        ts_msg = TransformStamped()
+        ts_msg.header.stamp = rospy.Time(0)
+        ts_msg.header.frame_id = parent_frame_id
+        ts_msg.child_frame_id = child_frame_id
+        ts_msg.transform.translation.x = t[0]
+        ts_msg.transform.translation.y = t[1]
+        ts_msg.transform.translation.z = t[2]
+        ts_msg.transform.rotation.x = q[0]
+        ts_msg.transform.rotation.y = q[1]
+        ts_msg.transform.rotation.z = q[2]
+        ts_msg.transform.rotation.w = q[3]
+        return ts_msg
+
+    def get_imprint(self, view=False):
+        # return the contact imprint
+        depth_r = self.depth_r['img']
+        depth_l = self.depth_l['img']
+        imprint_r = get_imprint_pc(self.references['right'], depth_r, threshold=self.threshold,
+                                   K=self.camera_info['right']['K'])
+        imprint_l = get_imprint_pc(self.references['left'], depth_l, threshold=self.threshold,
+                                   K=self.camera_info['left']['K'])
+        frame_r = self.depth_r['frame']
+        frame_l = self.depth_l['frame']
+
+        filtered_imprint_r = self.filter_pc(imprint_r)
+        filtered_imprint_l = self.filter_pc(imprint_l)
+
+        # trasform imprints
+        imprint_r = self._tr_pc(filtered_imprint_r, frame_r, self.reconstruction_frame)
+        imprint_l = self._tr_pc(filtered_imprint_l, frame_l, self.reconstruction_frame)
+
+        imprint = np.concatenate([imprint_r, imprint_l], axis=0)
+        return imprint
 
 
 if __name__ == '__main__':
@@ -62,7 +141,6 @@ if __name__ == '__main__':
     # predict next imprint
     next_imprint = model(imprint_t, action_t).squeeze()
 
-
     # Upsample ouptut
     sample_out = {
         'next_imprint':next_imprint.cpu().detach().numpy(),
@@ -83,7 +161,7 @@ if __name__ == '__main__':
     imprint_threshold = object_params['imprint_th']['depth']
     icp_threshold = object_params['icp_th']
 
-    reconstructor = BubblePCReconsturctorOfflineDepth(threshold=imprint_threshold, object_name=object_name, estimation_type='icp2d')
+    reconstructor = BubblePCReconstructorOfflineDepth(threshold=imprint_threshold, object_name=object_name, estimation_type='icp2d')
     # obtain camera parameters
     camera_info_r = sample['camera_info_r']
     camera_info_l = sample['camera_info_l']
@@ -107,6 +185,7 @@ if __name__ == '__main__':
     reconstructor.depth_l = deformed_depth_l
     reconstructor.camera_info['right'] = camera_info_r
     reconstructor.camera_info['left'] = camera_info_l
-    #           add also the tfs to the parsers's tf buffer
+    # compute transformations from camera frames to grasp frame and transform the
+    reconstructor.add_tfs(sample['all_tfs'])
     # estimate pose
-    estimated_pose = reconstructor.estimate_pose(icp_threshold)
+    estimated_pose = reconstructor.pose_estimator.estimate_pose(imprint)
