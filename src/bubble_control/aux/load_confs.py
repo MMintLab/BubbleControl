12ae579a32a4d030f37f6882914ff0556c943efe
mr-mikmik <aglaner@hotmail.com>
2022-01-10 11:48:56 -0500

    adding saved object models

--- src/bubble_control/aux/load_confs.py
+++ src/bubble_control/aux/load_confs.py
@@ -3,6 +3,7 @@ import sys
 import os
 import numpy as np
 import bubble_utils
+from mmint_camera_utils.point_cloud_utils import pack_o3d_pcd, unpack_o3d_pcd
 
 package_path = project_path = os.path.join(os.path.dirname(os.path.abspath(__file__)).split('/bubble_control')[0], 'bubble_control')
 
@@ -30,10 +31,17 @@ def load_object_models():
     bubble_icp_models_path = os.path.join(package_path, 'config', 'object_models.npy')
     with open(bubble_icp_models_path, 'rb') as f:
         object_models = np.load(f)
+
+    # pack object models as pcd
+    for k, ar_i in object_models.items():
+        object_models[k] = pack_o3d_pcd(ar_i)
     return object_models
 
 
 def save_object_models(object_models_dict):
     bubble_icp_models_path = os.path.join(package_path, 'config', 'object_models.npy')
+    # unpack object models to numpy arrays
+    for k,pcd_i in object_models_dict.items():
+        object_models_dict[k] = unpack_o3d_pcd(pcd_i)
     with open(bubble_icp_models_path, 'wb') as f:
         np.save(f, object_models_dict)
