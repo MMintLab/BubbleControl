quaternion-to-axis fixed

src/bubble_control/bubble_learning/aux/orientation_trs.py
@@ -49,7 +49,7 @@ class QuaternionToAxis(object):
             # numpy
             qw = x[..., -1]
             theta = 2 * np.arccos(qw)
-            theta = np.expand_dims(theta, axis=1).repeat(3, axis=-1)
+            theta = np.expand_dims(theta, axis=-1).repeat(3, axis=-1)
             axis = np.divide(x[..., :3], np.sin(theta/2), out=np.zeros_like(theta), where=theta!=0) # fix that when theta is 0, then axis is (0,0,0) (not defined)
             # NOTE: should be a unit vector
             x_tr = theta * axis
@@ -60,15 +60,16 @@ class QuaternionToAxis(object):
             theta = torch.norm(x_tr, dim=-1)
             theta = theta.unsqueeze(-1).repeat_interleave(3, axis=-1)
             axis = x_tr / theta
+            axis[theta==0] = 0
             qw = torch.cos(theta[...,0] * 0.5).unsqueeze(-1)
             qxyz = torch.sin(theta * 0.5) * axis
             x = torch.cat([qxyz, qw], dim=-1)
         else:
             theta = np.linalg.norm(x_tr, axis=-1)
             theta = np.expand_dims(theta, axis=-1).repeat(3, axis=-1)
-            axis = x_tr/theta
-            qw = np.cos(theta[..., 0]*0.5)
-            qxyz = np.sin(theta[..., 0]*0.5)*axis
+            axis = np.divide(x_tr,theta, out=np.zeros_like(theta), where=theta!=0)
+            qw = np.cos(theta[..., 0:1]*0.5)
+            qxyz = np.sin(theta*0.5)*axis
             x = np.concatenate([qxyz, qw], axis=-1)
         return x
 