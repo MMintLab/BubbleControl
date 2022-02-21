fixing quaternion-to-axis tr

src/bubble_control/bubble_learning/aux/orientation_trs.py
@@ -3,6 +3,7 @@ import numpy as np
 import tf.transformations as tr
 import pytorch3d.transforms as batched_trs
 
+
 class QuaternionToAxis(object):
 
     def __init__(self, keys_to_tr=None):
@@ -36,24 +37,39 @@ class QuaternionToAxis(object):
     def _tr(self, x):
         # transform a quaternion encoded rotation to an axis one with 3 values representing the axis of rotation where the modulus is the angle magnitude
         # q = [qx, qy, qz, qw] where qw = cos(theta/2); qx = a1*sin(theta/2),...
-        qw = x[..., -1]
-        theta = 2 * np.arccos(qw)
-        eps = np.array([1e-7], dtype=x.dtype)
-        if len(x.shape) == 2:
-            theta = np.expand_dims(theta, axis=1) 
-            eps = np.expand_dims(eps, axis=-1)       
-        axis = x[..., :3] / (np.sin(theta/2) + eps) # should be a unit vector
-        x_tr = theta * axis
+        if torch.is_tensor(x):
+            qw = x[..., -1]
+            theta = 2 * torch.arccos(qw)
+            theta = theta.unsqueeze(dim=-1).repeat_interleave(3, dim=-1)
+            axis = x[..., :3]/ torch.sin(theta / 2)
+            axis[theta==0] = 0  # filter out nans and infs from the division where theta is 0
+            # NOTE: should be a unit vector
+            x_tr = theta * axis
+        else:
+            # numpy
+            qw = x[..., -1]
+            theta = 2 * np.arccos(qw)
+            theta = np.expand_dims(theta, axis=1).repeat(3, axis=-1)
+            axis = np.divide(x[..., :3], np.sin(theta/2), out=np.zeros_like(theta), where=theta!=0) # fix that when theta is 0, then axis is (0,0,0) (not defined)
+            # NOTE: should be a unit vector
+            x_tr = theta * axis
         return x_tr
 
     def _tr_inv(self, x_tr):
-        theta = np.linalg.norm(x_tr, axis=-1)
-        if len(x_tr.shape) == 2:
-            theta = np.expand_dims(theta, axis=1)
-        axis = x_tr/theta
-        qw = np.cos(theta/2)
-        qxyz = np.sin(theta/2)*axis
-        x = np.append(qxyz, qw, axis=-1)
+        if torch.is_tensor(x_tr):
+            theta = torch.norm(x_tr, dim=-1)
+            theta = theta.unsqueeze(-1).repeat_interleave(3, axis=-1)
+            axis = x_tr / theta
+            qw = torch.cos(theta[...,0] * 0.5).unsqueeze(-1)
+            qxyz = torch.sin(theta * 0.5) * axis
+            x = torch.cat([qxyz, qw], dim=-1)
+        else:
+            theta = np.linalg.norm(x_tr, axis=-1)
+            theta = np.expand_dims(theta, axis=-1).repeat(3, axis=-1)
+            axis = x_tr/theta
+            qw = np.cos(theta[..., 0]*0.5)
+            qxyz = np.sin(theta[..., 0]*0.5)*axis
+            x = np.concatenate([qxyz, qw], axis=-1)
         return x
 
 
@@ -77,3 +93,26 @@ class EulerToAxis(object):
         euler = batched_trs.matrix_to_euler_angles(matrix, 'ZYX')
         euler_sxyz = torch.index_select(euler, dim=-1, index=torch.LongTensor([2,1,0]))
         return euler_sxyz
+
+
+# DEBUG
+if __name__ == '__main__':
+    q2a = QuaternionToAxis()
+    simple_sample = {
+        'quat': np.array([0, 0, 0, 1]),
+    }
+    numpy_sample = {
+        'quat': np.expand_dims(np.array([0, 0, 0, 1]), axis=0).repeat(5, axis=0),
+    }
+    tensor_sample = {
+        'quat': torch.tensor(np.array([0, 0, 0, 1])).unsqueeze(0).repeat_interleave(5, dim=0),
+    }
+    simple_sample_tr = q2a(simple_sample)
+    numpy_sample_tr = q2a(numpy_sample)
+    tensor_sample_tr = q2a(tensor_sample)
+
+    simple_sample_rec = q2a.inverse(simple_sample_tr)
+    numpy_sample_rec = q2a.inverse(numpy_sample_tr)
+    tensor_sample_rec = q2a.inverse(tensor_sample_tr)
+
+