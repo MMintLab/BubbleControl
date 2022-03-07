aac713aaa659868a2c13556dfbbd3bef82aedfb1
mr-mikmik <aglaner@hotmail.com>
2022-03-07 15:44:02 -0500

    abstracting out the imprint image grid and pose grid

--- src/bubble_control/bubble_learning/models/object_pose_dynamics_model.py
+++ src/bubble_control/bubble_learning/models/object_pose_dynamics_model.py
@@ -18,6 +18,7 @@ from bubble_control.bubble_learning.models.bubble_autoencoder import BubbleAutoE
 from bubble_control.bubble_learning.models.dynamics_model_base import DynamicsModelBase
 from bubble_control.bubble_learning.aux.orientation_trs import QuaternionToAxis
 from bubble_control.bubble_learning.aux.pose_loss import ModelPoseLoss
+from bubble_control.bubble_learning.aux.visualization_utils.pose_visualization import get_object_pose_images_grid
 
 
 class ObjectPoseDynamicsModel(DynamicsModelBase):
@@ -121,62 +122,6 @@ class ObjectPoseDynamicsModel(DynamicsModelBase):
         return loss
 
     def _log_object_pose_images(self, obj_pose_pred, obj_pose_gth, phase):
-        obj_trans_pred = obj_pose_pred[..., :3]
-        obj_rot_pred = obj_pose_pred[..., 3:]
-        obj_rot_angle_pred = self._get_angle_from_axis_angle(obj_rot_pred, self.plane_normal)
-        obj_trans_gth = obj_pose_gth[..., :3]
-        obj_rot_gth = obj_pose_gth[..., 3:]
-        obj_rot_angle_gth = self._get_angle_from_axis_angle(obj_rot_gth, self.plane_normal)
-        images = self._get_pose_images(obj_trans_pred, obj_rot_angle_pred, obj_trans_gth, obj_rot_angle_gth)
-        grid = torchvision.utils.make_grid(images)
+        grid = get_object_pose_images_grid(obj_pose_pred, obj_pose_gth, self.plane_normal)
         self.logger.experiment.add_image('pose_estimation_{}'.format(phase), grid, self.global_step)
 
-    def _get_angle_from_axis_angle(self, orientation, plane_normal):
-        if orientation.shape[-1] == 4:
-            q_to_ax = QuaternionToAxis()
-            axis_angle = torch.from_numpy(q_to_ax._tr(orientation.detach().numpy()))
-        else:
-            axis_angle = orientation
-        normal_axis_angle = torch.einsum('bi,i->b', axis_angle, plane_normal).unsqueeze(-1) * plane_normal.unsqueeze(0)
-        angle = torch.norm(normal_axis_angle, dim=-1)
-        if torch.sum(torch.isnan(angle)) != 0:
-            import pdb; pdb.set_trace()
-        return angle
-
-    def _get_pose_images(self, trans_pred, rot_angle_pred, trans_gth, rot_angle_gth):
-        images = []
-        for i in range(len(trans_pred)):
-            img = np.zeros([100, 100, 3], dtype=np.uint8)
-            img.fill(100)
-            pred_param = self._find_rect_param(trans_pred[i], rot_angle_pred[i], img)
-            color_p = (255, 0, 0)
-            self._draw_angled_rec(*pred_param, color_p, img)
-            gth_param = self._find_rect_param(trans_gth[i], rot_angle_gth[i], img)
-            color_gth = (0, 0, 255)
-            self._draw_angled_rec(*gth_param, color_gth, img)
-            img = torch.tensor(img)
-            img = img.permute(2, 0, 1)
-            images.append(img)
-        return images
-
-    def _find_rect_param(self, trans, rot, img):
-        height = 0.06 * 100 / 0.15
-        width = 0.015 * 100 / 0.15
-        center_x = img.shape[0] / 2 + trans[0] * 10 / 0.15
-        center_y = img.shape[1] / 2 + trans[1] * 10 / 0.15
-        return center_x, center_y, width, height, rot.item()
-
-    def _draw_angled_rec(self, x0, y0, width, height, angle, color, img):
-        b = np.cos(angle) * 0.5
-        a = np.sin(angle) * 0.5
-        pt0 = (int(x0 - a * height - b * width),
-            int(y0 + b * height - a * width))
-        pt1 = (int(x0 + a * height - b * width),
-            int(y0 - b * height - a * width))
-        pt2 = (int(2 * x0 - pt0[0]), int(2 * y0 - pt0[1]))
-        pt3 = (int(2 * x0 - pt1[0]), int(2 * y0 - pt1[1]))
-
-        cv2.line(img, pt0, pt1, color, 3)
-        cv2.line(img, pt1, pt2, color, 3)
-        cv2.line(img, pt2, pt3, color, 3)
-        cv2.line(img, pt3, pt0, color, 3)
