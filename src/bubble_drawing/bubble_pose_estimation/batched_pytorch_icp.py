ded51f3ec63e300e97db3def68a8b5cbd2e0342d
mr-mikmik <aglaner@hotmail.com>
2022-12-09 13:52:11 -0500

    moving pose estimation back to bubble_drawing

---
+++ src/bubble_drawing/bubble_pose_estimation/batched_pytorch_icp.py
@@ -0,0 +1,143 @@
+import torch
+import numpy as np
+from tqdm import tqdm
+
+
+def icp_2d_masked(pc_model, pc_scene, pc_scene_mask, num_iter=30):
+    # ICP 2D:
+    # pc_scene: (N, n_points, n_coords)
+    # pc_scene_mask: (N, n_points, n_coords)
+    # pc_model: (N, n_model_points, n_coords)
+
+    N, n_points, n_coords = pc_scene.shape
+    if len(pc_scene_mask.shape) == len(pc_scene_mask.shape)-1:
+        pc_scene_mask = pc_scene_mask.unsqueeze(-1).repeat_interleave(n_coords, dim=-1)  # (N, n_scene_points, n_coords)
+
+    R_init = torch.eye(n_coords, device=pc_scene.device, dtype=pc_scene.dtype).unsqueeze(0).repeat_interleave(N, dim=0)  # (N, num_dims, num_dims)--- init all R as identyty
+    t_init = masked_tensor_mean(pc_scene.transpose(1, 2), pc_scene_mask.transpose(1, 2),
+                                start_dim=-1)  # mean of the scene
+
+    R, t = R_init, t_init
+    for i in range(num_iter):
+        R, t = icp_2d_maksed_step(pc_model, pc_scene, pc_scene_mask, R_init, t_init)
+
+        R_init = R
+        t_init = t
+    # R: (N, n_coords, n_coords)
+    # t: (N, n_coords)
+    return R, t
+
+
+def icp_2d_masked_imprints(pc_model, pc_scene, pc_scene_mask, num_iter=30):
+    # ICP 2D:
+    # pc_scene: (N, n_impr, w, h, n_coords)
+    # pc_scene_mask: (N, n_impr, w, h, n_coords)
+    # pc_model: (N, n_model_points, n_coords)
+    # n_scene_points = n_impr * w * h
+
+    N, n_impr, w, h, n_coords = pc_scene.shape
+    pc_scene_r = reshape_pc(pc_scene)
+    pc_scene_mask_r = reshape_pc(pc_scene_mask)
+    R, t = icp_2d_masked(pc_model, pc_scene_r, pc_scene_mask_r, num_iter=num_iter)
+    # R: (N, n_coords, n_coords)
+    # t: (N, n_coords)
+    return R, t
+
+
+def icp_2d_maksed_step(pc_model, pc_scene, pc_scene_mask, R_init, t_init):
+    # pc_model, shape (N, n_model_points, n_coords)
+    # pc_scene, shape (N, n_scene_points, n_coords)
+    # pc_scene_mask, shape (N, n_scene_points, n_coords) *** Here n_coords dimension is just repeated
+    # t_init: (N, n_coords)
+    # R_init: (N, n_coords, n_coords)
+    # -------------------
+    # transform init:
+    pc_model_tr = pc_batched_tr(pc_model, R_init, t_init)
+
+    # Estimate correspondences (only masked):
+    # compute distances and get minimums
+    batch_idxs, corr_indxs = estimate_correspondences_batched(pc_model_tr, pc_scene, pc_scene_mask)
+    pc_model_selected = pc_model[batch_idxs, corr_indxs, :]
+
+    # Compute new transform
+    R_star, t_star = find_best_transform_batched_masked(pc_model_selected, pc_scene, pc_scene_mask)
+
+    return R_star, t_star
+
+
+def masked_tensor_mean(xs, masks, start_dim=-2):
+    # masked mean along the last start_dim dims
+    masks_f = masks.flatten(start_dim=start_dim)
+    xs_f = xs.flatten(start_dim=start_dim)
+    num_occ = torch.sum(masks_f, dim=-1)
+    xs_sum = torch.sum(masks_f*xs_f, dim=-1)
+    xs_mean_f = xs_sum/num_occ
+    xs_mean = xs_mean_f
+    return xs_mean
+
+
+def pc_batched_tr(pc, R, t):
+    # pc: (batch_size, n_points, n_coords)
+    # R: (batch_size, n_coords, n_coords)
+    # t: (batch_size, n_coords)
+    pc_rot = torch.einsum('kij,klj->kli', R, pc)  # (batch_size, n_points, n_coords)
+    pc_tr = (pc_rot.transpose(0, 1) + t).transpose(0, 1)  # (batch_size, n_points, n_coords)
+    return pc_tr
+
+
+def reshape_pc(pc):
+    # pc: (N, n_impr, w, h, n_coords)
+    N, n_impr, w, h, n_coords = pc.shape
+    # _pc_r = pc.permute(0,1,3,4,2)
+    pc_r = pc.reshape(N, n_impr * w * h, n_coords)  # (N, n_impr*w*h, n_coords)
+    return pc_r
+
+
+def estimate_correspondences_batched(a1, a2, a2_mask):
+    """
+    Return for each point in the scene (a2) the closest point in the model (a1)
+    :param a1: (N, n_1_points, n_coords) -- model
+    :param a2: (N, n_2_points, n_coords) -- scene
+    :param a2_mask: (N, n_2_points, n_coords)
+    :return: tensor containing the correspndent model points for each scene points (N, n_2_points, n_coords)
+    """
+    # Compute distances
+    N1, n_1_points, n_coords_1 = a1.shape
+    N2, n_2_points, n_coords_2 = a2.shape
+
+    dists = (torch.sum((a1 ** 2), dim=-1).unsqueeze(-1) -
+             torch.bmm(a1, a2.transpose(1, 2)) * 2 +
+             torch.sum((a2 ** 2), dim=-1).unsqueeze(-2)).transpose(1, 2)
+
+    # Get clossest point indxs
+    corr_indxs = torch.argmin(dists, axis=-1)  # get a1 index that minimizes distance to a2
+
+    # Apply correspondences
+    batch_idxs = torch.arange(0, corr_indxs.shape[0]).unsqueeze(-1).repeat_interleave(n_2_points, dim=-1)
+    return batch_idxs, corr_indxs
+
+
+def find_best_transform_batched_masked(pc_model, pc_scene, pc_mask):
+    # pc_model: (N, n_scene_points, n_coords) -- model
+    # pc_scene: (N, n_scene_points, n_coords) -- scene
+    # pc_mask: (N, n_scene_points, n_coords)
+    N, n_points, n_coords = pc_scene.shape
+    #     pc_mask_extended = pc_mask.unsqueeze(-1).repeat(1,1, n_coords)# (N, n_scene_points, n_coords)
+
+    mu_s = masked_tensor_mean(pc_scene.transpose(1, 2), pc_mask.transpose(1, 2), start_dim=-1)  # (N, n_coords)
+    mu_m = masked_tensor_mean(pc_model.transpose(1, 2), pc_mask.transpose(1, 2), start_dim=-1)  # (N, n_coords)
+    p_s = (pc_scene.transpose(0, 1) - mu_s).transpose(0, 1)  # (N, n_scene_points, n_coords)
+    p_m = (pc_model.transpose(0, 1) - mu_m).transpose(0, 1)  # (N, n_scene_points, n_coords)
+    # apply mask
+    ps_filtered = p_s * pc_mask
+    pm_filtered = p_m * pc_mask
+    W = torch.einsum('kij,kil->kjl', ps_filtered, pm_filtered)  # (N, n_coords, n_coords)
+    U, S, Vh = torch.linalg.svd(W)
+    R = U @ Vh
+    detR = torch.det(R)  # (N,)
+    Vh[:, -1] = Vh[:, -1] * torch.stack([detR] * 2, dim=-1)
+    R_star = U @ Vh
+    t_star = mu_s - torch.einsum('kij,kj->ki', R_star, mu_m)
+    return R_star, t_star
+
+
---
+++ src/bubble_drawing/bubble_pose_estimation/bubble_pose_estimation.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+
+import tf
+import threading
+import copy
+
+from visualization_msgs.msg import Marker
+
+from mmint_camera_utils.camera_utils.point_cloud_utils import *
+from wsg_50_utils.wsg_50_gripper import WSG50Gripper
+
+from bubble_drawing.bubble_pose_estimation.bubble_pc_reconstruction import BubblePCReconsturctorDepth, BubblePCReconsturctorTreeSearch
+
+
+class BubblePoseEstimator(object):
+    """
+    BubblePoseEstimation > BubblePCReconstructor > PoseEstimators
+    """
+
+    def __init__(self, imprint_th=0.005, icp_th=0.01, rate=5.0, percentile=None, view=False, verbose=False, broadcast_imprint=False, object_name='allen', estimation_type='icp3d', reconstruction='depth', gripper_width=None):
+        self.object_name = object_name
+        self.imprint_th = imprint_th
+        self.icp_th = icp_th
+        self.rate = rate
+        self.percentile = percentile
+        self.view = view
+        self.verbose = verbose
+        self.broadcast_imprint = broadcast_imprint
+        self.estimation_type = estimation_type
+        self.gripper_width = gripper_width
+        try:
+            rospy.init_node('bubble_pose_estimator')
+        except (rospy.exceptions.ROSInitException, rospy.exceptions.ROSException):
+            pass
+        self.gripper = WSG50Gripper()
+        self.reconstructor = self._get_reconstructor(reconstruction)
+        self.marker_publisher = rospy.Publisher('estimated_object', Marker, queue_size=100)
+        self.tf_broadcaster = tf.TransformBroadcaster()
+        self.tool_estimated_pose = None
+        self.alive = True
+        self.calibrate()
+        self.lock = threading.Lock()
+        self.publisher_thread = threading.Thread(target=self._marker_publishing_loop)
+        self.publisher_thread.start()
+        self.estimate_pose(verbose=self.verbose)
+        rospy.spin()
+
+    def _get_reconstructor(self, reconstruction_key):
+        reconstructors = {
+            'depth': BubblePCReconsturctorDepth,
+            'tree': BubblePCReconsturctorTreeSearch,
+        }
+        if reconstruction_key not in reconstructors:
+            raise KeyError('No reconstructor found for key {} -- Possible keys: {}'.format(reconstruction_key, reconstructors.keys()))
+        Reconstructor = reconstructors[reconstruction_key]
+        reconstructor = Reconstructor(threshold=self.imprint_th, object_name=self.object_name, estimation_type=self.estimation_type,
+                              view=self.view, verbose=self.verbose, broadcast_imprint=self.broadcast_imprint, percentile=self.percentile)
+        return reconstructor
+
+    def calibrate(self):
+        info_msg = 'Press enter to calibrate --'
+        if self.gripper_width is not None:
+            info_msg += '\n\t>>> We will open the gripper!\t'
+        _ = input(info_msg)
+        if self.gripper_width is not None:
+            # Open the gripper
+            self.gripper.open_gripper()
+        self.reconstructor.reference()
+        info_msg = 'Calibration done! {}\nPress enter to continue :)'
+        additional_msg = ''
+        if self.gripper_width is not None:
+            additional_msg = '\n We will close the gripper to a width {}mm'.format(self.gripper_width)
+        _ = input(info_msg.format(additional_msg))
+        if self.gripper_width is not None:
+            # move gripper to gripper_width
+            self.gripper.move(self.gripper_width, speed=50.0)
+
+    def estimate_pose(self, verbose=False):
+        rate = rospy.Rate(self.rate)
+        while not rospy.is_shutdown():
+            try:
+                icp_tr = self.reconstructor.estimate_pose(threshold=self.icp_th, view=self.view, verbose=verbose)
+                with self.lock:
+                    # update the tool_estimated_pose
+                    t = icp_tr[:3, 3]
+                    q = tr.quaternion_from_matrix(icp_tr)
+                    self.tool_estimated_pose = np.concatenate([t, q])
+            except rospy.ROSInterruptException:
+                self.finish()
+                break
+            rate.sleep()
+
+    def _marker_publishing_loop(self):
+        publish_rate = rospy.Rate(self.rate)
+        while not rospy.is_shutdown():
+            with self.lock:
+                current_tool_pose = copy.deepcopy(self.tool_estimated_pose)
+            if current_tool_pose is not None:
+                marker_i = self._create_marker(current_tool_pose[:3], current_tool_pose[3:])
+                self.marker_publisher.publish(marker_i)
+            publish_rate.sleep()
+            with self.lock:
+                if not self.alive:
+                    return
+
+    def _create_marker(self, t, q):
+        mk = Marker()
+        mk.header.frame_id = self.reconstructor.reconstruction_frame
+        mk.type = Marker.CYLINDER
+        mk.scale.x = 2*self.reconstructor.radius
+        mk.scale.y = 2*self.reconstructor.radius
+        mk.scale.z = 2*self.reconstructor.height # make it larger
+        # set color
+        mk.color.r = 158/255.
+        mk.color.g = 232/255.
+        mk.color.b = 217/255.
+        mk.color.a = 1.0
+        # set position
+        mk.pose.position.x = t[0]
+        mk.pose.position.y = t[1]
+        mk.pose.position.z = t[2]
+        mk.pose.orientation.x = q[0]
+        mk.pose.orientation.y = q[1]
+        mk.pose.orientation.z = q[2]
+        mk.pose.orientation.w = q[3]
+        return mk
+
+    def finish(self):
+        with self.lock:
+            self.alive = False
+        self.publisher_thread.join()
+
+
+if __name__ == '__main__':
+
+    # Continuous  pose estimator:
+    # view = False
+    view = True
+    # imprint_th = 0.0048 # for pen with gw 15
+    # imprint_th = 0.0048 # for allen with gw 12
+    imprint_th = 0.0053 # for marker with gw 20
+    # imprint_th = 0.006 # for spatula with gripper width of 15mm
+    icp_th = 1. # consider all points
+    icp_th = 0.005 # for allen key
+
+    bpe = BubblePoseEstimator(view=view, imprint_th=imprint_th, icp_th=icp_th, rate=5., verbose=view)
